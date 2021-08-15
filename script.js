40637cf1cdf3b1faf7dac941e980d56887b0a2bd
kateshyshkalova <77622318+kateshyshkalova@users.noreply.github.com>
2021-08-15 23:31:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let text = prompt('Введіть текст');
+console.log(text);
+let count = 0;
+
+let pos = 0;
+while (true) {
+    let foundPos = text.indexOf('html', pos);
+    if (foundPos == -1) break;
+
+    console.log(` Знайдено тут: ${foundPos} `);
+    count++;
+    pos = foundPos + 1; // продолжаем со следующей позиции
+}
+console.log(count);
